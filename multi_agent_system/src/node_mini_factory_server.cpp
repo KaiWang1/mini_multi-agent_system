d989e37b47a599c05eb9d2e48a6d6abe3853d597
kai_linux <872625203@qq.com>
2019-02-13 11:41:14 +0800

    step2

---
+++ multi_agent_system/src/node_mini_factory_server.cpp
@@ -0,0 +1,25 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "multi_agent_system/node_mini_factory_server.h"
+
+node_mini_factory_server_def::node_mini_factory_server_def()
+{
+	feedback_sub = n.subscribe("/agent_feedback",10,&node_mini_factory_server_def::agent_feedback_callback,this);
+}
+node_mini_factory_server_def::~node_mini_factory_server_def()
+{
+
+}
+void node_mini_factory_server_def::agent_feedback_callback(const multi_agent_system::agent_feedback::ConstPtr& msg)
+{
+	ROS_INFO("RECEIVE A MESSAGE!");
+}
+
+int main(int argc, char *argv[])
+{
+	ros::init(argc, argv, "node_mini_factory_server");
+
+	ros::spin();
+
+	return 0;
+}
