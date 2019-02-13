f897c1cdc5852b6fbfd8de5bb6e18f2423cf3c94
kai_linux <872625203@qq.com>
2019-02-13 15:15:58 +0800

    step6

--- multi_agent_system/include/multi_agent_system/node_robot_2.h
+++ multi_agent_system/include/multi_agent_system/node_robot_2.h
@@ -2,6 +2,15 @@
 #include "ros/ros.h"
 #include "multi_agent_system/agent_task_2.h"
 
+
+#define robot_clock 0.1
+#define robot_task_cost 3
+#define robot_timer_count_limit 1.0/robot_clock*robot_task_cost
+enum robot_state
+{
+    ready,
+    executing
+};
 class robot_2_def{
 public:
     robot_2_def();
@@ -10,4 +19,9 @@ public:
 private:
     ros::NodeHandle n;
     ros::ServiceServer task_2_server;
+    ros::Timer robot_timer;
+    robot_state robot_2_state;
+    unsigned int timer_count;
+
+    void time_callback(const ros::TimerEvent&);
 };
--- multi_agent_system/src/node_robot_2.cpp
+++ multi_agent_system/src/node_robot_2.cpp
@@ -5,6 +5,7 @@
 robot_2_def::robot_2_def()
 {
 	task_2_server = n.advertiseService("/agent_task_2",&robot_2_def::task_2_function,this);
+	robot_timer = n.createTimer(ros::Duration(robot_clock), &robot_2_def::time_callback,this);
 }
 robot_2_def::~robot_2_def()
 {
@@ -12,9 +13,28 @@ robot_2_def::~robot_2_def()
 }
 bool robot_2_def::task_2_function(multi_agent_system::agent_task_2::Request &req,multi_agent_system::agent_task_2::Response &res)
 {
-	ROS_INFO("TASK2 IS CALLED!");
+	ROS_INFO("TASK2 IS CALLED! CLIENT ID=%d",req.ID);
+	if(req.cmd == "executing"){
+		robot_2_state = executing;
+		res.robot_state = "executing";
+	}
+	else
+		res.robot_state = "error";
 	return true;
 }
+void robot_2_def::time_callback(const ros::TimerEvent&)
+{
+	if(robot_2_state == executing){
+		timer_count++;
+		if(timer_count >= robot_timer_count_limit){
+			robot_2_state = ready;
+			timer_count = 0;
+		}
+	}
+	else
+		timer_count = 0;
+}
+
 int main(int argc, char *argv[])
 {
 	ros::init(argc, argv, "node_robot_2");
